7b63bcbbd84914ac9bdee9c68636a872cc70e0e7
tejasv <tejasv@cdac.in>
2024-12-18 17:59:56 +0530

    refactor: simplify CORS middleware configuration

--- src/index.js
+++ src/index.js
@@ -20,12 +20,7 @@ const notesRoutes = require('./routes/notes');
 const app = express();
 
 // Middleware
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(express.json());
 
 // Database connection
